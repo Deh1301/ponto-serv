f33c02551eb8ecbeccd104043000591a2561ed47
Deh1301 <calixtoandre77@gmail.com>
2026-04-10 16:19:58 -0300

    atualizando projeto

--- public/js/main.js
+++ public/js/main.js
@@ -17,3 +17,26 @@ function toggleCard(card){
 
   card.classList.toggle('active')
 }
+
+function enviarWhats(tipo){
+
+  let numero = "5512999999999"; // troca aqui
+
+  let mensagem = "";
+
+  if (tipo === "ar") {
+    mensagem = "Olá! Aqui é da Pontoserv Assistência Técnica. Vi no site sobre higienização de ar condicionado. Quantos aparelhos são, quantos BTUs e qual bairro?";
+  }
+
+  if (tipo === "geladeira") {
+    mensagem = "Olá! Aqui é da Pontoserv Assistência Técnica. Vi no site sobre manutenção de geladeira, vou descrever o defeito:";
+  }
+
+  if (tipo === "lavaeseca") {
+    mensagem = "Olá! Aqui é da Pontoserv Assistência Técnica. Vi no site sobre manutenção de lava e seca, e lavadouras de roupa vou descrever o defeito:";
+  }
+
+  let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
+  window.open(url, "_blank");
+}
